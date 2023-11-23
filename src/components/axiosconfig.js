dfba771cb6439c17130e74278826d2eff2c83dc8
Surya <suryaschypfer@gmail.com>
2023-11-23 18:20:29 -0500

    removed data source button for first q

--- src/components/axiosconfig.js
+++ src/components/axiosconfig.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 
-const baseURL = "http://3.138.183.163:3000";
+const baseURL = "http://3.15.156.179:3000";
 // const baseURL = "http://localhost:3000";
 const axiosInstance = axios.create({
   baseURL,
