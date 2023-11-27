51d678c01c216f1d458af2438e69a3bebd32d576
Surya <suryaschypfer@gmail.com>
2023-11-27 10:45:57 -0500

    changes required

--- src/components/config.js
+++ src/components/config.js
@@ -1,1 +1,1 @@
-export const baseUrl = "http://localhost:3000";
+export const baseUrl = "http://18.189.11.24:3000";
--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-export const baseUrl = "http://localhost:3000";
+export const baseUrl = "http://18.189.11.24:3000";
 // config.ts
 // const apiUrlBase = "http://3.133.102.189:3000";
-const apiUrlBase = "http://localhost:3000";
+const apiUrlBase = "http://18.189.11.24:3000";
 // export { apiUrlBase };
