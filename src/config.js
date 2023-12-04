c790de25419f4e3df8d692f54e220ea1b5801f9e
Surya <suryaschypfer@gmail.com>
2023-12-03 23:20:54 -0500

    Miscellaneous

--- src/config.js
+++ src/config.js
@@ -1,5 +1,2 @@
-export const baseUrl = "http://localhost:3000";
-// config.ts
-// const apiUrlBase = "http://3.133.102.189:3000";
-const apiUrlBase = "http://localhost:3000";
-// export { apiUrlBase };
+export const baseUrl = "http://18.224.17.131:3000";
+const apiUrlBase = "http://18.224.17.131:3000";
